8726f5445ecb8f1928123b4eb49ac68618ed821c
Tyler Barber <dev@twbarber.com>
2017-11-03 19:49:05 -0500

    Remove config and settings, using ENV vars now. Properly package Fat Jar.

--- README.md
+++ README.md
@@ -6,9 +6,16 @@ Lambda executable `jar` to send Slack messages via Slack Incoming Webhook.
 
 Written using Kotlin and Gradle.
 
-## Demployment Instructions
+## Build
 
 - Run `./gradlew jar` to build your `jar`.
+
+## Deploy for use with AWS API Gateway
+
 - Upload `/build/distributions/slack-send-lambda-1.0-SNAPSHOT.zip` to a new Lambda Function
-- Set the Handler of the function to `Main::handler`
-- The Logs should show your Lambda host's IP Address
+- Set the Handler of the function to `com.twbarber.slack.Main::apiGatewayHandler`
+
+## Deploy for use with AWS SNS
+
+- Upload `/build/distributions/slack-send-lambda-1.0-SNAPSHOT.zip` to a new Lambda Function
+- Set the Handler of the function to `com.twbarber.slack.Main::snsHandler`
--- src/main/kotlin/com/twbarber/slack/Handlers.kt
+++ src/main/kotlin/com/twbarber/slack/Handlers.kt
@@ -1,25 +1,30 @@
 package com.twbarber.slack
 
 import com.amazonaws.services.lambda.runtime.Context
-import com.amazonaws.services.lambda.runtime.events.SNSEvent
-import com.natpryce.konfig.ConfigurationProperties
-import com.twbarber.slack.config.settings.slackWebhookUrl
 import java.io.InputStream
 import java.io.OutputStream
 
-fun snsHandler(request: SNSEvent, context: Context) {
-	val logger = context.logger
-	val config = loadConfig()
-	logger.log("Configuration Loaded...")
-	SlackService.send(context, "", config[slackWebhookUrl])
-}
 
-fun apiGatewayHandler(request: InputStream, response: OutputStream, context: Context) {
-	val logger = context.logger
-	val config = loadConfig()
-	logger.log("Configuration Loaded...")
-	SlackService.send(context, "", config[slackWebhookUrl])
-}
+class Main {
 
-private fun loadConfig(): ConfigurationProperties =
-	ConfigurationProperties.fromResource("application.properties")
+	companion object {
+		private const val WEBHOOK_URL_KEY = "webhookUrl"
+	}
+
+	fun snsHandler(context: Context): String {
+		val logger = context.logger
+		val webhookUrl = System.getenv(WEBHOOK_URL_KEY)
+		logger.log("SNS Configuration Loaded... $webhookUrl")
+		return "snsHandler Complete"
+		// SlackService.send(context, "", config[slackWebhookUrl])
+	}
+
+	fun apiGatewayHandler(request: InputStream, response: OutputStream, context: Context): String {
+		val logger = context.logger
+		val webhookUrl = System.getenv(WEBHOOK_URL_KEY)
+		logger.log("API Gateway Configuration Loaded... $webhookUrl")
+		return response.write("apiGatewayHandler Complete".toByteArray(Charsets.UTF_8)).toString()
+		// SlackService.send(context, "", config[slackWebhookUrl])
+	}
+
+}
